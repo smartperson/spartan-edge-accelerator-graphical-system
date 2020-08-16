e94b9ceae0057a95e8b66f79195db4066363af6c
Varun Mehta <varun@mehtasw.com>
2020-08-16 17:44:23 -0400

    Broken multiple 16-bit color layers.
    Debugging to work through the issues that cause left-right jitter when scrolling bg layer.

--- hdmi_display_controller_1.srcs/sources_1/new/bg_compositor.v
+++ hdmi_display_controller_1.srcs/sources_1/new/bg_compositor.v
@@ -21,126 +21,179 @@
 
 
 module bg_compositor
-    #( parameter  RAM_READ_START_CYCLE    = 5'b11001 )
+    #(  parameter  RAM_READ_START_CYCLE    = 5'b11001,
+        parameter  RAM_MAP_ADDR            = 15'h2000
+    )
     (
-    input wire [15:0] i_x,
-    input wire [15:0] i_y,
+    input wire [15:0] i_x_raw,
+    input wire [15:0] i_y_raw,
     input wire i_v_sync,
     input wire i_pix_clk,
     input wire [15:0] i_in_data,
+    input wire i_btn,
+    output wire o_ram_enable,
     output wire [14:0] o_addr,
     
-    output wire [7:0] o_red,
-    output wire [7:0] o_green,
-    output wire [7:0] o_blue,
-    output wire o_bg_hit
+    output wire [2:0] o_palette,
+    output wire [3:0] o_color,
+    output wire o_priority
     );
 
-    reg [15:0] sprite_x     = 16'd00;
-    reg [15:0] sprite_y     = 16'd00;
-    reg sprite_x_flip       = 0;
+//    reg [15:0] sprite_x     = 16'd00;
+//    reg [15:0] sprite_y     = 16'd00;
     reg sprite_y_flip       = 0;
     wire sprite_hit_x, sprite_hit_y;
     wire [2:0] sprite_render_x;
     wire [2:0] sprite_render_y;
     
-    localparam [0:15][0:15][2:0][7:0] palette_colors =  {
-        384'hDFDFDFE8F0F800581020680040780060880088A000A8B80098E0E0000000D8381858F85898E0E0000000E8F0F8F85858,
-        384'hDFDFDFE8F0F8F8F8F800000000B8F800C8F800E0F800F8F898E0E000000058A8F0F8F8F898E0E0000000D8A038F8D870,
-        384'hDFDFDFE8F0F8000000786818C89858007848E0C05000C800000000F8F8F800000000C800B00000F80000F85800F8A000,
-        384'hDFDFDFE8F0F8000000585858787878989898C0C0C0E0E0E0000000F8F8F800000000C800E81868F040A8F878C8F8C0F0,
-        384'hDFDFDFE8F0F8707070000000C0C0C0A0C8F8A8E0F8C0F8F8000000F8F8F800000000C80000E00088F838C8F800F8F898,
-        384'hDFDFDFE8F0F8000000B8A860D8F8C800800000C80000F800000000000000000000000000000000000000000000000000,
-        384'hDFDFDFE8F0F8000000885818F808F8D8A038F8D820F8F800000000000000000000000000000000000000000000000000,
-        384'hDFDFDFE8F0F8000000B80050F800804848886868B08080C8000000000000000000000000000000000000000000000000,
-        384'hDFDFDFF8F8F8000000885818D8A038F8D870F8D0C0E800B0500000F8407020308840809880D8C8B02860F87068F8F800,
-        384'hDFDFDFF8F8F8000000707070A0A0A0C0C0C0E0E0E0F81058000000F8F8F800000000C800B00000F80000F85800F8A000,
-        384'hDFDFDFF8F8F8000000F87800F8C000F8F800B82800F88800000000F8F8F800000000C800E81868F040A8F878C8F8C0F0,
-        384'hDFDFDFF8F8F80000004040D86868D88888F8B82800F88800000000F8F8F800000000C80000E00088F838C8F800F8F898,
-        384'hDFDFDFF8F8F8000000880000B80000F80000B82800F88800000000000000000000000000000000000000000000000000,
-        384'hDFDFDFF8F8F800000000780000B80000F800B82800F88800000000000000000000000000000000000000000000000000,
-        384'hDFDFDFF8F8F8000000283048485058686858989040C0C078000000000000000000000000000000000000000000000000,
-        384'hDFDFDFF8F8F818484820706828887830A08838B898F80080000000000000000000000000000000000000000000000000
-    };
+    reg [15:0] i_x, i_y;
+    reg [10:0] x_scroll = 0;
+    reg [9:0] y_scroll;
+    reg [3:0] counter;
+
+    //assign i_x = i_x_raw + x_scroll;
+    assign i_y = i_y_raw + y_scroll;
+    assign i_x = i_x_raw + x_scroll;
+    
+    reg curr_tile_index;
+    always @(negedge i_pix_clk) begin
+        curr_tile_index <= (((i_x[4:0]+1) & 31) >= (x_scroll & 31)) ? 0 : 1; //>=? 
+    end 
+    
+    always @(posedge i_v_sync) begin
+        if (RAM_MAP_ADDR == 15'h2000)
+            counter <= counter+1;
+//        else
+//            if (x_scroll < 2048)
+//                x_scroll <= x_scroll+1;
+//            else
+//                x_scroll <= 0;
+    end
+
+    always @(posedge counter[3]) begin
+        if (RAM_MAP_ADDR == 15'h2000)
+            if (x_scroll < 2048)
+                x_scroll <= x_scroll+1;
+            else
+                x_scroll <= 0;
+    end
+    
+//    always @(posedge i_btn) begin
+//        if (RAM_MAP_ADDR == 15'h2000)
+//            x_scroll <= x_scroll+1;
+//    end
+
     
     assign sprite_hit_x = 1;
     assign sprite_hit_y = 1;
     
-    assign sprite_render_x = i_x[4:2];
+    assign sprite_render_x = i_x[4:2]; //TODO is this still right? Causing problems?
     assign sprite_render_y = i_y[4:2];
     reg [2:0] selected_palette;
     reg [3:0] selected_color;
 
+    reg reg_ram_enable;
     reg [14:0]reg_addr;
     //reg [15:0]reg_ram;
     reg [9:0]reg_tile_number;
-    reg [7:0]reg_tile_attr;
-    reg [5:0]reg_next_tile_attrs;
-    reg [0:31] reg_next_tile;
-    reg [0:31] reg_curr_tile;
+    reg [0:2][7:0]reg_tile_attr;
+    reg [0:2][0:31] reg_tile_data;
+        
     wire [1:0]x_attr_bits;
     assign x_attr_bits = i_x[6:5];
     always @(negedge i_pix_clk) begin
-        case (i_x[4:0])
-            5'b11111: begin
-                sprite_y_flip <= reg_next_tile_attrs[5];
-                sprite_x_flip <= reg_next_tile_attrs[4];
-                // bit 3 has priority, figure out how/what to assign it to here
-            end
-        endcase
-        if (sprite_x_flip)
-            selected_color <= {reg_curr_tile[(7-sprite_render_x)+24], reg_curr_tile[(7-sprite_render_x)+16], reg_curr_tile[(7-sprite_render_x)+8], reg_curr_tile[(7-sprite_render_x)]};
-        else
-            selected_color <= {reg_curr_tile[sprite_render_x+24], reg_curr_tile[sprite_render_x+16], reg_curr_tile[sprite_render_x+8], reg_curr_tile[sprite_render_x]};
+//        case (i_x_raw[4:0])
+//            5'b11111: begin
+//                sprite_y_flip <= reg_tile_attr[curr_tile_index+1][5];
+//                // bit 3 has priority, figure out how/what to assign it to here
+//            end
+//        endcase
+//        if (sprite_x_flip)
+//            selected_color <= {reg_tile_data[curr_tile_index][(7-sprite_render_x)+24],
+//                               reg_tile_data[curr_tile_index][(7-sprite_render_x)+16],
+//                               reg_tile_data[curr_tile_index][(7-sprite_render_x)+8],
+//                               reg_tile_data[curr_tile_index][(7-sprite_render_x)]
+//                               };
+//        else
+            selected_color <= {reg_tile_data[curr_tile_index][sprite_render_x+24],
+                               reg_tile_data[curr_tile_index][sprite_render_x+16],
+                               reg_tile_data[curr_tile_index][sprite_render_x+8],
+                               reg_tile_data[curr_tile_index][sprite_render_x]
+                               };
     end
     always @(negedge i_pix_clk) begin
-        case (i_x[4:0])
-//            5'b10111: reg_addr <= 0;//15'h23C0 + (i_x == 16'b1111111111110111 ? i_y[15:5]*10 : (((i_x[15:5]+1)>>2) + i_y[15:5]*10));
-//            5'b11000: ; //reg_next_tile_attr <= 0;//i_in_data;
+        case (i_x_raw[4:0])
             RAM_READ_START_CYCLE: begin
-                if (i_x[10])
-                    reg_addr <= 15'h2400 | (i_y[15:5]*32 + i_x[9:5]/*+1*/);
+                reg_ram_enable <= 1;
+                if ((i_x + 64) >= 1024 && i_x_raw < 1280)
+                    reg_addr <= RAM_MAP_ADDR+15'h400 + i_y[15:5]*32 + i_x[9:5] + 2 + 32;// + (i_x_raw > 1280 ? -32 : 0); // i_x[9:5]? +1?
                 else
-                    reg_addr <= 15'h2000 | (i_x == 16'b1111111111111001 ? i_y[15:5]*32 : (i_y[15:5]*32 + i_x[15:5]/*+1*/));
+                    reg_addr <= RAM_MAP_ADDR+15'h000 + i_y[15:5]*32 + 2 + i_x[9:5] + ((i_x_raw+64) > 1280  ? +0 : +32); //: (i_y[15:5]*32 + 2));
             end
             RAM_READ_START_CYCLE+1: begin
+//                reg_ram_enable <= 0;
                 reg_tile_number <= i_in_data[9:0];
-                reg_next_tile_attrs <= i_in_data[15:10];
+                reg_tile_attr[2] <= i_in_data[15:10];
             end
             RAM_READ_START_CYCLE+2: begin
-                if (reg_next_tile_attrs[5])
+                reg_ram_enable <= 1;
+                if (reg_tile_attr[2][5]) //y flip?
                     reg_addr <= 15'b000000000000000 + (reg_tile_number*16) + (7-sprite_render_y);
                 else
                     reg_addr <= 15'b000000000000000 + (reg_tile_number*16) + sprite_render_y;
             end
-            RAM_READ_START_CYCLE+3: reg_next_tile[0:15] <= i_in_data;
+            RAM_READ_START_CYCLE+3: begin
+                if (reg_tile_attr[2][4]) begin //x flip?
+                    integer i;
+                    for(i=0;i<8;i=i+1)
+                        reg_tile_data[2][i] = i_in_data[i+8]; //i_in_data[7-i];
+                    for(i=8;i<16;i=i+1)
+                        reg_tile_data[2][i] = i_in_data[i-8]; //i_in_data[7-i+16];
+                end
+                else
+                    reg_tile_data[2][0:15] <= i_in_data;
+            end
             RAM_READ_START_CYCLE+4: begin
-                if (reg_next_tile_attrs[5])
+                if (reg_tile_attr[2][5]) // y flip?
                     reg_addr <= 15'b000000000000000 + (reg_tile_number*16) + (7 -sprite_render_y + 8);
                 else
                     reg_addr <= 15'b000000000000000 + (reg_tile_number*16) + sprite_render_y + 8;
             end
-            RAM_READ_START_CYCLE+5: reg_next_tile[16:31] <= i_in_data;
+            RAM_READ_START_CYCLE+5: begin
+                if (reg_tile_attr[2][4]) begin//x flip
+                    integer i;
+                    for(i=16;i<24;i=i+1)
+                        reg_tile_data[2][i] = i_in_data[i-16+8]; //i_in_data[7-i+16];
+                    for(i=24;i<32;i=i+1)
+                        reg_tile_data[2][i] = i_in_data[i-16-8]; //i_in_data[7-i+32];
+                end
+                else
+                    reg_tile_data[2][16:31] <= i_in_data;
+            end
+            RAM_READ_START_CYCLE+6: begin
+                reg_ram_enable <= 0;
+            end
             5'b11111: begin //this one should always be done on the last clock cycle for the tile
-                reg_curr_tile <= reg_next_tile;
+                reg_ram_enable <= 0;
+                reg_tile_data[0] = reg_tile_data[1];
+                reg_tile_data[1] = reg_tile_data[2];
+                reg_tile_attr[0] = reg_tile_attr[1];
+                reg_tile_attr[1] = reg_tile_attr[2];
             end
             default: ; 
         endcase
     end
+    assign o_ram_enable = reg_ram_enable;
     assign o_addr   = reg_addr;
 //    assign o_red    = (sprite_hit_x && sprite_hit_y) ? palette_colors[selected_palette][selected_color][2] : 8'hXX;
 //    assign o_green  = (sprite_hit_x && sprite_hit_y) ? palette_colors[selected_palette][selected_color][1] : 8'hXX;
 //    assign o_blue   = (sprite_hit_x && sprite_hit_y) ? palette_colors[selected_palette][selected_color][0] : 8'hXX;
     reg [7:0] reg_red, reg_green, reg_blue;
-    assign o_red    = reg_red;
-    assign o_green  = reg_green;
-    assign o_blue   = reg_blue;
-    assign o_bg_hit = 1;
+    assign o_palette    = selected_palette;
+    assign o_color  = selected_color;
+    assign o_priority   =  1; //TODO incorporate selected_priority;
     always @(posedge i_pix_clk) begin
-        reg_red    = (sprite_hit_x && sprite_hit_y) ? palette_colors[selected_palette][selected_color][2] : 8'hXX;
-        reg_green  = (sprite_hit_x && sprite_hit_y) ? palette_colors[selected_palette][selected_color][1] : 8'hXX;
-        reg_blue   = (sprite_hit_x && sprite_hit_y) ? palette_colors[selected_palette][selected_color][0] : 8'hXX;
-        if (i_x[4:0] == 5'b11111)
-            selected_palette <= reg_next_tile_attrs[2:0];
+//        if (i_x_raw[4:0] == 5'b11111)
+            selected_palette <= reg_tile_attr[curr_tile_index][2:0]; //+1?
     end
 endmodule
