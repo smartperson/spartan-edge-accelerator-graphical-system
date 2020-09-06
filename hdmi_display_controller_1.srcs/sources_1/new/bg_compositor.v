6a5a6f1f7dd03de06b43ce22912cb817f94e20b4
Varun Mehta <varun@mehtasw.com>
2020-09-06 14:50:30 -0400

    x- and y-scrolling of both background layers
    current hardware design moves y and and down while scrolling continuously to the right on x. Takes care of critical needs for #6 , but scroll values do not get read from RAM yet.

--- hdmi_display_controller_1.srcs/sources_1/new/bg_compositor.v
+++ hdmi_display_controller_1.srcs/sources_1/new/bg_compositor.v
@@ -44,32 +44,45 @@ module bg_compositor
     reg sprite_y_flip       = 0;
     wire sprite_hit_x, sprite_hit_y;
     reg [2:0] sprite_render_x;
-    wire [2:0] sprite_render_y;
+    reg [2:0] sprite_render_y;
     
     reg [15:0] i_x, i_y;
     reg [13:0] x_scroll = 0;
-    reg [9:0] y_scroll;
+    reg [9:0] y_scroll = (RAM_MAP_ADDR == 15'h2000) ? 304 : 304;
+    reg y_dir = 0; //0 go up, 1 go down
     reg [3:0] counter;
 
     assign i_x = i_x_raw + x_scroll;
-    assign i_y = i_y_raw + y_scroll;
+    assign i_y = (i_y_raw + y_scroll)%1024;
 //    assign i_x = i_x_raw + x_scroll;
     
     reg curr_tile_index;
 //    assign curr_tile_index = (((i_x+1) & 31) >= (x_scroll & 31)) ? 1 : 0;
     
     always @(posedge i_v_sync) begin
-        if (RAM_MAP_ADDR == 15'h2000)
+        if (RAM_MAP_ADDR == 15'h2000) begin
+            if (y_scroll == 0)
+                y_dir <= 1;
+            else if (y_scroll == 1024-720)
+                y_dir <= 0;
             counter <= counter+2;
-        else
+        end
+        else begin
+            if (y_scroll == 152)
+                y_dir <= 1;
+            else if (y_scroll == 304)
+                y_dir <= 0;
             counter <= counter+1;
+        end
     end
 
     always @(posedge counter[1]) begin //@(posedge i_y_raw[0]) begin
-        if (x_scroll < 10240)
-            x_scroll <= x_scroll+1;
-        else
-            x_scroll <= 0;
+//        if (x_scroll < 10240)
+//            x_scroll <= x_scroll+1;
+//        else
+//            x_scroll <= 0;
+        y_scroll <= y_scroll + (y_dir ? +1: -1);
+        x_scroll <= x_scroll+1;
     end
     
 //    always @(posedge i_y_raw[0]) begin
@@ -90,7 +103,6 @@ module bg_compositor
     assign sprite_hit_y = 1;
     
 //    assign  //TODO is this still right? Causing problems?
-    assign sprite_render_y = i_y_raw[4:2];
     reg [2:0] selected_palette;
     reg [3:0] selected_color;
 
@@ -118,6 +130,7 @@ module bg_compositor
         curr_tile_index = (((i_x[4:0]) & 31) >= (x_scroll & 31)) ? 0 : 1; //+1? //>=?
         selected_palette = reg_tile_attr[curr_tile_index][2:0];
         sprite_render_x = i_x[4:2];
+        sprite_render_y = i_y[4:2];
         selected_color = {reg_tile_data[curr_tile_index][sprite_render_x+24],
                           reg_tile_data[curr_tile_index][sprite_render_x+16],
                           reg_tile_data[curr_tile_index][sprite_render_x+8],
